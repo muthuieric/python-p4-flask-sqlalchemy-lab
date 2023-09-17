aa290f736553d86172dcd7f2f8dd8084fac69239
Eric <ericmuthuipatch22@gmail.com>
2023-09-17 19:10:41 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -17,18 +17,93 @@ db.init_app(app)
 def home():
     return '<h1>Zoo app</h1>'
 
+
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
 
+    if not animal:
+        response_body = '<h1>404 animal not found</h1>'
+        response = make_response(response_body, 404)
+        return response    
+
+    response_body = f'''
+        <h1>Animal View</h1>
+        <ul>
+            <li>ID: {animal.id}</li>
+            <li>Name: {animal.name}</li>
+            <li>Species: {animal.species}</li>
+            <li>Zookeeper: {animal.zookeeper.name}</li>
+            <li>Enclosure: {animal.enclosure.environment}</li>
+        </ul>
+    '''
+    response = make_response(response_body, 200)
+
+    return response 
+
+
+    # return ''
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+
+    if not zookeeper:
+        response_body = '<h1>404 zookeeper not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    animal_list = [
+        f'<li>ID: {zookeeper.id}</li>',
+        f'<li>Name: {zookeeper.name}</li>',
+        f'<li>Birthday: {zookeeper.birthday}</li>'
+    ]
+
+    for animal in zookeeper.animals:
+        animal_list.append(f'<li>Animal: {animal.name}</li>')
+
+    response_body = f'''
+    <h1>Zookeeper View</h1>
+    <ul>
+        {''.join(animal_list)}
+    </ul>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
 
+    if not enclosure:
+        response_body = '<h1>404 enclosure not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+
+
+    animal_list2 = [
+        f'<li>ID: {enclosure.id}</li>',
+        f'<li>Environment: {enclosure.environment}</li>',
+        f'<li>Open to Visitors: {enclosure.open_to_visitors}</li>'
+    ]
+
+    for animal in enclosure.animals:
+        animal_list2.append(f'<li>Animal: {animal.name}</li>')
+
+    response_body = f'''
+    <h1>Enclosure View</h1>
+    <ul>
+        {''.join(animal_list2)}
+    </ul>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
+   
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from datetime import date
+
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -11,13 +13,30 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, default="Unknown")
+    birthday = db.Column(db.Date, nullable=False, default=date(2000, 1, 1))
+    
+    # Establish a one-to-many relationship with animals
+    animals = db.relationship('Animal', backref='zookeeper', lazy=True)
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(255), nullable=True)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+
+    # Establish a one-to-many relationship with animals
+    animals = db.relationship('Animal', backref='enclosure', lazy=True)
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    species = db.Column(db.String(255), nullable=False)
+
+    # Define foreign key relationships
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
